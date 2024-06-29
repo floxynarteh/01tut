7be294ba9ca5b7c225fb1828abc712967905fe8e
floxy narteh <floxy.narteh@thecobaltpartners.com>
2024-06-29 07:48:52 +0200

    changed 3 files

--- src/App.js
+++ src/App.js
@@ -3,38 +3,61 @@ import Header from "./Header";
 import { useState } from "react";
 import Footer from "./Footer";
 import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-    },
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
 
-    { id: 2, checked: false, item: "Item 2" },
-    { id: 3, checked: false, item: "Item 3" },
-  ]);
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItem) => {
+    setItems(newItem);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItem));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    //addItem
+    addItem(newItem);
+    setNewItem("");
   };
 
   return (
     <div className="App">
       <Header title="Groceries" />
-      <AddItem />
+
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
